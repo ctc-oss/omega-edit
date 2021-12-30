8f6fb9677b57e0a93631d5b926b0eca514512371
Davin Shearer <davin.shearer@evc.ctc.com>
2021-12-30 14:11:31 -0500

    remove duplicate license blurb

--- src/examples/omega_version.js
+++ src/examples/omega_version.js
@@ -14,24 +14,11 @@
  * limitations under the License.                                                                                     *
  **********************************************************************************************************************/
 
-/**********************************************************************************************************************
- * Copyright (c) 2021 Concurrent Technologies Corporation.                                                            *
- *                                                                                                                    *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance     *
- * with the License.  You may obtain a copy of the License at                                                         *
- *                                                                                                                    *
- *     http://www.apache.org/licenses/LICENSE-2.0                                                                     *
- *                                                                                                                    *
- * Unless required by applicable law or agreed to in writing, software is distributed under the License is            *
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or                   *
- * implied.  See the License for the specific language governing permissions and limitations under the License.       *
- *                                                                                                                    *
- **********************************************************************************************************************/
-
 /*
  * Uses Omega Edit to display its version information.  Example from src/examples:
  * node ./omega_version.js
  */
+
 omega_edit = require('../../module/omega_edit')
 console.log("omega edit version: " + omega_edit.omega_version_major() + "." + omega_edit.omega_version_minor() + "." + omega_edit.omega_version_patch())
 console.log("omega edit version number: " + omega_edit.omega_version())
