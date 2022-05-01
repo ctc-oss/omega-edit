329273cdccb16e898653b2d9b24c680868489747
Davin Shearer <2205472+scholarsmate@users.noreply.github.com>
2022-05-01 13:25:34 -0400

    prep for 0.9.2-rc1 (#260)

--- src/rpc/client/ts/compile-proto.sh
+++ src/rpc/client/ts/compile-proto.sh
@@ -12,20 +12,14 @@
 # implied.  See the License for the specific language governing permissions and limitations under the License.  
 
 
-# OUT_DIR="../client/types/omega-edit"
-# TS_OUT_DIR="../client/types/omega-edit"
-OUT_DIR="../client/ts"
-TS_OUT_DIR="../client/ts"
+OUT_DIR="$PWD"
+TS_OUT_DIR="$PWD"
 IN_DIR="../../protos"
 PROTOC="$(yarn bin)/grpc_tools_node_protoc"
 PROTOC_GEN_TS_PATH="$(yarn bin)/protoc-gen-ts"
 PROTOC_GEN_GRPC_PATH="$(yarn bin)/grpc_tools_node_protoc_plugin"
 
-cd $IN_DIR
-
-# mkdir -p ${OUT_DIR} || true
-# mkdir -p ${TS_OUT_DIR} || true
-
+pushd $IN_DIR
 $PROTOC \
     --plugin=protoc-gen-ts=${PROTOC_GEN_TS_PATH} \
     --plugin=protoc-gen-grpc=${PROTOC_GEN_GRPC_PATH} \
@@ -33,5 +27,4 @@ $PROTOC \
     --grpc_out=grpc_js:${OUT_DIR} \
     --ts_out=grpc_js:${TS_OUT_DIR} \
     omega_edit.proto
-
-cd $current_dir
+popd
