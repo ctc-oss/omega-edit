90d8200a578ac8e7ff30c1c456b1544499eb4856
Davin Shearer <scholarsmate@gmail.com>
2022-05-16 18:29:54 -0400

    fix rat check

--- project/Rat.scala
+++ project/Rat.scala
@@ -26,6 +26,7 @@ object Rat {
     file("packaging/.cpack_ignore"),
     // json files
     file("src/rpc/client/ts/package.json"),
+    file("src/rpc/client/ts/package-lock.json"),
     file("src/rpc/client/ts/tests/package.json"),
     file("src/rpc/client/ts/tests/package-lock.json"),
     file("src/rpc/client/ts/tests/tsconfig.json"),
