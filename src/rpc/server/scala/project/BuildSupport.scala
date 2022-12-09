5f8de4d82819e9983a6791332beb2eea8c55ad69
Shane Dell <shanedell100@gmail.com>
2022-12-08 20:42:32 -0500

    Add support for aarch, eg mac m1

--- src/rpc/server/scala/project/BuildSupport.scala
+++ src/rpc/server/scala/project/BuildSupport.scala
@@ -37,6 +37,7 @@ object BuildSupport {
   val Win: Regex = """windows.+""".r
   val Amd: Regex = """amd(\d+)""".r
   val x86: Regex = """x86_(\d+)""".r
+  val aarch: Regex = """aarch(\d+)""".r
 
   // https://stackoverflow.com/a/51416386
   def filterScopedDependenciesFromPom(node: XmlNode): XmlNode =
@@ -70,6 +71,7 @@ object BuildSupport {
     val arch = System.getProperty("os.arch").toLowerCase match {
       case Amd(bits) => bits
       case x86(bits) => bits
+      case aarch(bits) => bits
       case arch      => throw new IllegalStateException(s"unknown arch: $arch")
     }
     Platform(os, arch)
@@ -85,6 +87,7 @@ object BuildSupport {
     val arch = System.getProperty("os.arch").toLowerCase match {
       case Amd(bits) => bits
       case x86(bits) => bits
+      case aarch(bits) => bits
       case arch      => throw new IllegalStateException(s"unknown arch: $arch")
     }
     Arch(s"$os-$arch", s"${os}_$arch", s"$os", s"$arch")
