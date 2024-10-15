36d57dbee8fb3c689614ce8b9b6599154f0145d7
John Wass <wassj@ctc.com>
2024-10-15 17:17:10 -0400

    Fix copyright date
    New unit test in #1007 added incorrect copyright date in the header.

--- server/scala/serv/src/test/scala/com/ctc/omega_edit/grpc/Bug976Spec.scala
+++ server/scala/serv/src/test/scala/com/ctc/omega_edit/grpc/Bug976Spec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Concurrent Technologies Corporation
+ * Copyright 2021 Concurrent Technologies Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
