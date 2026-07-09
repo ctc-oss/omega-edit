0adb212edee1a57aca1e23a8a153ba89a035125e
Davin Shearer <2205472+scholarsmate@users.noreply.github.com>
2026-07-08 22:05:23 -0400

    feat: add byte hover tooltip details (#1523)

---
+++ vscode-extension/webview-ui/src/components/ByteTooltip.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  interface Props {
+    title: string
+    alignRight?: boolean
+    below?: boolean
+    showAccent?: boolean
+  }
+
+  let {
+    title,
+    alignRight = false,
+    below = false,
+    showAccent = false,
+  }: Props = $props()
+
+  const lines = $derived(title.split('\n'))
+</script>
+
+<span
+  class="byte-tooltip"
+  class:alignRight
+  class:below
+  class:showAccent
+  aria-hidden="true"
+>
+  {#each lines as line}
+    <span>{line}</span>
+  {/each}
+</span>
